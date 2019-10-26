3c43fdb45688929ad71c8328b9a686eeb8601b9d
Scott Zhao <zhaomh1998@outlook.com>
2019-10-26 11:46:58 -0700

    slack bot clean up messages

--- SlackBot/bot_test.py
+++ SlackBot/bot_test.py
@@ -5,12 +5,42 @@ import asyncio
 import concurrent
 from datetime import datetime
 from tokens import slack_token
+import traceback
+import pickle
+import re
+import html
+
+# Clean up message
+RE_BRACKET = "\<.*?\>"  # Direct Mentions
+RE_COLON = "\:.*?\:"  # Emoji
+STR_REMOVE = '*_~`'
+RE_REMOVE = [RE_BRACKET, RE_COLON]
 
 
 @slack.RTMClient.run_on(event='message')
 async def say_hello(**payload):
     data = payload['data']
-    print(f"USER={data['user']} on CHANNEL={data['channel']}, said:{data['text']}")
+    is_message = 'client_msg_id' in data.keys()
+    if is_message:
+        try:
+            user_id = data['user']
+            channel_id = data['channel']
+            user_message = clean_msg(data['text'])
+            print(f"{user_message}<-USER={user_id}\tCH={channel_id}")
+        except:
+            traceback.print_exc()
+            pickle.dump(data, open("error_data.pkl", 'wb'))
+
+
+def clean_msg(message_str):
+    # Delete direct mentions and emoji
+    for re_expr in RE_REMOVE:
+        message_str = re.sub(re_expr, '', message_str)
+    # Delete special formatting characters
+    for i in STR_REMOVE:
+        message_str = message_str.replace(i, '')
+    # HTML unescape
+    return html.unescape(message_str)
 
 
 def sync_loop():
